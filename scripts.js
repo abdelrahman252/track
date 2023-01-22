d45dc55e1831aec5625496ade0d8b850fec651ff
Abdelrahman Ahmed <73317105+abdelrahman252@users.noreply.github.com>
2023-01-22 21:14:00 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,114 @@
+let nav = document.querySelector("nav");
+let btn = document.querySelector(".buttn");
+
+// button to top
+window.onscroll = function () {
+  if (window.scrollY >= 50) {
+    nav.classList.add("scrolled");
+    btn.style.display = "block";
+  } else {
+    nav.classList.remove("scrolled");
+    btn.style.display = "none";
+  }
+};
+
+btn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+// navBar
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector("ul");
+const links = document.querySelectorAll("ul li");
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+
+  hamburger.classList.toggle("toggle");
+});
+
+const header = document.querySelectorAll(".animate");
+console.log(header);
+
+window.addEventListener("scroll", animate);
+
+animate();
+
+function animate() {
+  const trigger = window.innerHeight / 5 * 4;
+  header.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < trigger) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+
+// cursor
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e",
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function (e) {
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+});
+
+function animateCircles() {
+  let x = coords.x;
+  let y = coords.y;
+
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+
+    circle.style.scale = (circles.length - index) / circles.length;
+
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
